a1b572210aa26a0e166b806dfc7ae88d0292c309
mygithub45 <47448298+mygithub45@users.noreply.github.com>
2023-02-15 23:29:55 +0530

    Add files via upload

---
+++ LeetCode/LongestPalindromicSubstring.java
@@ -0,0 +1,57 @@
+package LeetCode;
+
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s) {
+        int strLength = s.length();
+
+        if (strLength < 2) {
+            return s;
+        }
+
+        int resultLength = 0;
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            //Odd length
+            int left = i, right = i;
+            while (
+                    left >= 0 &&
+                            right < s.length() &&
+                            s.charAt(left) == s.charAt(right)
+            ) {
+                if ((right - left + 1) > resultLength) {
+                    result = s.substring(left, right + 1);
+                    resultLength = right - left + 1;
+                }
+                left -= 1;
+                right += 1;
+            }
+
+            // even length
+            left = i;
+            right = i + 1;
+            while (
+                    left >= 0 &&
+                            right < s.length() &&
+                            s.charAt(left) == s.charAt(right)
+            ) {
+                if ((right - left + 1) > resultLength) {
+                    result = s.substring(left, right + 1);
+                    resultLength = right - left + 1;
+                }
+                left -= 1;
+                right += 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main (String[] args)
+    {
+
+        System.out.println(longestPalindrome("forgeeksskeegfor"));
+        System.out.println(longestPalindrome("abbaeae"));
+
+    }
+}

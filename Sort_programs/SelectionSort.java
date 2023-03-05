e1dfaa64e4516ea5f4584b413674047034b38c60
mygithub45 <47448298+mygithub45@users.noreply.github.com>
2023-03-05 20:42:53 +0530

    Add files via upload

---
+++ Sort_programs/SelectionSort.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 1, 2,12,15,8,100,76,43,87,6,9};
+        // starting time
+        long start = System.currentTimeMillis();
+
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+        // ending time
+        long end = System.currentTimeMillis();
+        System.out.println("time taken " +
+                (end - start) + "ms");
+    }
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+        static void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+
+        static int getMaxIndex(int[] arr, int start, int end) {
+            int max = start;
+            for (int i = start; i <= end; i++) {
+                if (arr[max] < arr[i]) {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+}
